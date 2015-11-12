00d0b8068202309af69cd125d07c779b97f8f31b
VenkatramanC <venkatraman.c9@gmail.com>
2015-11-12 15:30:24 +0530

    Blogs aded to the page

--- app.js
+++ app.js
@@ -1,61 +1,78 @@
 //MODULES
- var knowEmo = angular.module('knowEmo', ['ngRoute']);
+var knowEmo = angular.module('knowEmo', []);
 //ROUTES
-    knowEmo.config(function ($routeProvider) {
-      
-    $routeProvider
+    //knowEmo.config(function ($routeProvider) {
+
+    // $routeProvider
     
-    .when('/' , {
-      templateUrl: 'pages/home.html',
-      controller: 'homeController'
-    })
+    // .when('/' , {
+    //   templateUrl: 'pages/home.html',
+    //   controller: 'homeController'
+    // })
 
-    .when('/about' , {
-      templateUrl: 'pages/about.html',
-      controller: 'aboutController'
-    })
-
-
-    .when('/blog' , {
-      templateUrl: 'pages/blog.html',
-      controller: 'blogController'
-    })
+    // .when('/about' , {
+    //   templateUrl: 'pages/about.html',
+    //   controller: 'aboutController'
+    // })
 
 
-    .when('/contact' , {  
-      templateUrl: 'pages/contact.html',
-      controller: 'contactController'
-    })
-
-    .when('/kids' , {  
-      templateUrl: 'pages/kids.html',
-      controller: 'kidsController'
-    })
-
-    .when('/boys' , {
-      templateUrl: 'pages/boys.html',
-      controller: 'boysController'
-    })
-    
-    .when('/login' , {
-      templateUrl: 'pages/login.html',
-      controller: 'loginController'
-    })
-
-    .when('/blog' , {
-      templateUrl: 'pages/blog.html',
-      controller: 'blogController'
-    })
+    // .when('/blog' , {
+    //   templateUrl: 'pages/blog.html',
+    //   controller: 'blogController'
+    // })
 
 
+    // .when('/contact' , {  
+    //   templateUrl: 'pages/contact.html',
+    //   controller: 'contactController'
+    // })
 
-});
+    // .when('/kids' , {  
+    //   templateUrl: 'pages/kids.html',
+    //   controller: 'kidsController'
+    // })
+
+    // .when('/boys' , {
+    //   templateUrl: 'pages/boys.html',
+    //   controller: 'boysController'
+    // })
+
+//     .when('/login' , {
+//       templateUrl: 'pages/login.html',
+//       controller: 'loginController'
+//     })
+
+//     .when('/blog' , {
+//       templateUrl: 'pages/blog.html',
+//       controller: 'blogController'
+//     })
+
+
+
+// });
 //CONTROLLERS 
-   
 
-   knowEmo.controller("homeController" , function() {
-       
-   });
+
+angular.module('knowEmo')
+  .controller('BlogCtrl' , function() {
+    var blogCtrl = this;
+    blogCtrl.title = 'blogs';
+    blogCtrl.blogs = [];
+
+    blogCtrl.initializeNewBlog = function(){
+      blogCtrl.newBlog = {};
+     }
+
+     blogCtrl.addBlog = function(){
+      blogCtrl.blogs.push(blogCtrl.newBlog);
+
+      blogCtrl.initializeNewBlog();
+      console.log(blogCtrl.blogs);
+    };
+
+    blogCtrl.initializeNewBlog();
+
+  });
 
 
  //knowEmo.controller("blogController" , function() {
@@ -78,8 +95,11 @@
 
 //});
 
- //knowEmo.controller("blogController" , function() {
+ // knowEmo.controller("blogController" , function() {
 
-//});
+
+
+ // });
+
 
 
